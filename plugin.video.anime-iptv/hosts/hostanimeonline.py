48e6d48d8e31e6ff1a3ac4cac24175ed63c34347
huball@ymail.com <huball@ymail.com@8c3795ff-bc63-d91a-1955-13f34479ecb5>
2014-12-23 08:07:44 +0000

    fix mp4upload host

--- plugin.video.anime-iptv/hosts/hostanimeonline.py
+++ plugin.video.anime-iptv/hosts/hostanimeonline.py
@@ -136,7 +136,7 @@ def Browse_PlayAnime(url, page='', content='episodes', view='515'):
     if idx2 == -1:
         return
     htmllink = html[idx:idx2]
-    htmllink = htmllink.lower()
+    htmllink = htmllink.replace("IFRAME SRC", "iframe src")
     r = re.compile('src="(.+?)"').findall(htmllink)
     ItemCount = len(r)
     if len(r) > 0:

e053366c720bbfc85e7d52253262e2e2daa200c0
huball@ymail.com <huball@ymail.com@8c3795ff-bc63-d91a-1955-13f34479ecb5>
2015-02-27 21:13:15 +0000

    fix

--- trunk/plugin.video.Pac-12/default.py
+++ trunk/plugin.video.Pac-12/default.py
@@ -1,94 +1,101 @@
 # -*- coding: utf-8 -*-
+import urllib
 import xbmcplugin
 import xbmcgui
 import xbmcaddon
 import sys
-mainSite = 'http://tvgry.pl/'
+import xbmc
+
 my_addon = xbmcaddon.Addon('plugin.video.Pac-12')
 addonPath = my_addon.getAddonInfo('path')
 icon = addonPath + '/icon.png'
 fanart = addonPath + '/fanart.jpg'
 
 
-addon_handle = int(sys.argv[1])
+def addDir(name, url, mode, iconimage, fanart, description):
+        u = sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+"&iconimage="+urllib.quote_plus(iconimage)+"&fanart="+urllib.quote_plus(fanart)
+        ok = True
+        liz = xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+        liz.setInfo(type="Video", infoLabels={"Title": name, "Plot": description})
+        liz.setProperty("Fanart_Image", fanart)
+        ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=u, listitem=liz, isFolder=True)
+        return ok
 
-xbmcplugin.setContent(addon_handle, 'movies')
 
-# Pac National
-url = 'http://goo.gl/0CofPI'
-li = xbmcgui.ListItem('Pac National 720p', iconImage=icon)
-li.setProperty('fanart_image', fanart)
-xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+def CATEGORIES():
+    addDir('Pac National', 'http://xrxs.net/video/live-p12netw-', 1, icon, fanart, '')
+    addDir('Arizona', 'http://xrxs.net/video/live-p12ariz-', 1, 'http://x.pac-12.com/profiles/pac12/themes/pac12_foundation/images/pac12/networks/network-arizona.jpg', fanart, '')
+    addDir('Bay Area', 'http://xrxs.net/video/live-p12baya-', 1, 'http://x.pac-12.com/profiles/pac12/themes/pac12_foundation/images/pac12/networks/network-bayarea.jpg', fanart, '')
+    addDir('Los Angeles', 'http://xrxs.net/video/live-p12losa-', 1, 'http://x.pac-12.com/profiles/pac12/themes/pac12_foundation/images/pac12/networks/network-losangeles.jpg', fanart, '')
+    addDir('Mountain', 'http://xrxs.net/video/live-p12moun-', 1, 'http://x.pac-12.com/profiles/pac12/themes/pac12_foundation/images/pac12/networks/network-mountain.jpg', fanart, '')
+    addDir('Oregon', 'http://xrxs.net/video/live-p12oreg-', 1, 'http://x.pac-12.com/profiles/pac12/themes/pac12_foundation/images/pac12/networks/network-oregon.jpg', fanart, '')
+    addDir('Washington', 'http://xrxs.net/video/live-p12wash-', 1, 'http://x.pac-12.com/profiles/pac12/themes/pac12_foundation/images/pac12/networks/network-washington.jpg', fanart, '')
 
-url = 'http://xrxs.net/video/live-p12netw-4728.m3u8'
-li = xbmcgui.ListItem('Pac National 1080p', iconImage=icon)
-li.setProperty('fanart_image', fanart)
-xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
 
-# Arizona
-url = 'http://goo.gl/yy2Y0F'
-li = xbmcgui.ListItem('Arizona 720p', iconImage=icon)
-li.setProperty('fanart_image', fanart)
-xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+def getItemTitles(table):
+    out = []
+    for i in range(len(table)):
+        value = table[i]
+        out.append(value[0])
+    return out
 
-url = 'http://xrxs.net/video/live-p12ariz-4728.m3u8'
-li = xbmcgui.ListItem('Arizona 1080p', iconImage=icon)
-li.setProperty('fanart_image', fanart)
-xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
 
-# Bay Area
-url = 'http://goo.gl/gHG3ty'
-li = xbmcgui.ListItem('Bay Area 720p', iconImage=icon)
-li.setProperty('fanart_image', fanart)
-xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+def Play(url, name, icon):
+    lista = [['1080p', '4728.m3u8'], ['720p', '2328.m3u8']]
+    d = xbmcgui.Dialog()
+    item = d.select("Wybór jakości", getItemTitles(lista))
+    print str(item)
+    if item != -1:
+        url = url + str(lista[item][1])
+        li = xbmcgui.ListItem(label=name, iconImage=icon, thumbnailImage=icon, path="")
+        xbmc.Player().play(item=url, listitem=li)
+    exit()
 
-url = 'http://xrxs.net/video/live-p12baya-4728.m3u8'
-li = xbmcgui.ListItem('Bay Area 1080p', iconImage=icon)
-li.setProperty('fanart_image', fanart)
-xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
 
-# Los Angeles
-url = 'http://goo.gl/m4Xaul'
-li = xbmcgui.ListItem('Los Angeles 720p', iconImage=icon)
-li.setProperty('fanart_image', fanart)
-xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+def get_params():
+        param = []
+        paramstring = sys.argv[2]
+        if len(paramstring) >= 2:
+                params = sys.argv[2]
+                cleanedparams = params.replace('?', '')
+                if (params[len(params) - 1] == '/'):
+                        params = params[0:len(params) - 2]
+                pairsofparams = cleanedparams.split('&')
+                param = {}
+                for i in range(len(pairsofparams)):
+                        splitparams = {}
+                        splitparams = pairsofparams[i].split('=')
+                        if (len(splitparams)) == 2:
+                                param[splitparams[0]] = splitparams[1]
+        return param
 
-url = 'http://xrxs.net/video/live-p12losa-4728.m3u8'
-li = xbmcgui.ListItem('Los Angeles 1080p', iconImage=icon)
-li.setProperty('fanart_image', fanart)
-xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
 
-# Mountain
-url = 'http://goo.gl/6QnKIG'
-li = xbmcgui.ListItem('Mountain 720p', iconImage=icon)
-li.setProperty('fanart_image', fanart)
-xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+params = get_params()
+url = None
+name = None
+mode = None
 
-url = 'http://xrxs.net/video/live-p12moun-4728.m3u8'
-li = xbmcgui.ListItem('Mountain 1080p', iconImage=icon)
-li.setProperty('fanart_image', fanart)
-xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+try:
+        url = urllib.unquote_plus(params["url"])
+except:
+        pass
+try:
+        name = urllib.unquote_plus(params["name"])
+except:
+        pass
+try:
+        mode = int(params["mode"])
+except:
+        pass
 
-# Oregon
-url = 'http://goo.gl/FLfuDu'
-li = xbmcgui.ListItem('Oregon 720p', iconImage=icon)
-li.setProperty('fanart_image', fanart)
-xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+print "Mode: "+str(mode)
+print "URL: "+str(url)
+print "Name: "+str(name)
 
-url = 'http://xrxs.net/video/live-p12oreg-4728.m3u8'
-li = xbmcgui.ListItem('Oregon1080p', iconImage=icon)
-li.setProperty('fanart_image', fanart)
-xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+if mode == None or url == None or len(url) < 1:
+        print ""
+        CATEGORIES()
 
-# Washington
-url = 'http://goo.gl/ZOww80'
-li = xbmcgui.ListItem('Washington 720p', iconImage=icon)
-li.setProperty('fanart_image', fanart)
-xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
-
-url = 'http://xrxs.net/video/live-p12wash-4728.m3u8'
-li = xbmcgui.ListItem('Washington 1080p', iconImage=icon)
-li.setProperty('fanart_image', fanart)
-xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
-
-xbmcplugin.endOfDirectory(addon_handle)
+elif mode == 1:
+        Play(url, name, icon)
+xbmcplugin.endOfDirectory(int(sys.argv[1]))

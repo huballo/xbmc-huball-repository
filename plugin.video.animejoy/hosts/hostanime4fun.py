9e51eacb3cf819e9b8cc8a01ac4af183b190623e
huball <huball@ymail.com>
2015-08-12 20:57:40 +0200

    fix

--- plugin.video.animejoy/hosts/hostanime4fun.py
+++ plugin.video.animejoy/hosts/hostanime4fun.py
@@ -25,8 +25,7 @@ def Anime4funlist(name, url):
         req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
         response = urllib2.urlopen(req)
         html = response.read()
-        response.close()
-        r = re.compile('<a href="http://www.anime4fun.com/category/anime-list/(.+?)">'+ name + '(.+?)</a>').findall(html)
+        r = re.compile('/category/anime-list/(.+?)">'+ name + '(.+?)</a>').findall(html)
         for url, name2 in r:
             url = 'http://www.anime4fun.com/category/anime-list/' + url
             plot = ''
@@ -50,9 +49,11 @@ def Anime4funEpisodes(url):
             return
         html = html[idx:idx2]
         html = html.replace('\'', '')
-        r = re.compile('<a href="(.+?)"><li class=(.+?)>(.+?)<span class=').findall(html)
+        html = html.replace('\n', '')
+        html = html.replace(' ', '')
+        r = re.compile('<ahref="(.+?)"><liclass=(.+?)>(.+?)<spanclass=').findall(html)
         for url, xx, name in r:
-            url = url
+            url = "http://animeonlinetv.tv" + url
             name = name
             plot = ''
             img = icon2

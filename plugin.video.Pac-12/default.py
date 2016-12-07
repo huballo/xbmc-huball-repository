2378730c17862018e4b1cdb32f847dbd2818b250
huball <pawel.malinski@sg.sangroup.pl>
2016-12-07 12:21:25 +0100

    fixes

--- plugin.video.Pac-12/default.py
+++ plugin.video.Pac-12/default.py
@@ -16,6 +16,7 @@ iconbtn = addonPath + '/iconbtn.png'
 fanartbtn = addonPath + '/fanartbtn.jpg'
 intro = addonPath + '/intro.mp4'
 isplayed = xbmc.getInfoLabel("Window(Home).Property(intro.isplayed)").lower() == "true"
+site = 'http://xrxs.net/process.php'
 
 
 def CATEGORIES():
@@ -24,13 +25,13 @@ def CATEGORIES():
             li = xbmcgui.ListItem(label='PAC-12', iconImage=icon, thumbnailImage=icon)
             xbmc.Player().play(intro, li)
             xbmcgui.Window(10000).setProperty("intro.isplayed", "true")
-    addDir('Pac National', my_addon.getSetting("National"), 1, icon, fanart, 'Pac-12')
-    addDir('Arizona', my_addon.getSetting("Arizona"), 1, 'http://x.pac-12.com/profiles/pac12/themes/pac12_foundation/images/pac12/networks/network-arizona.jpg', fanart, 'Pac-12 Arizona')
-    addDir('Bay Area', my_addon.getSetting("Bay"), 1, 'http://x.pac-12.com/profiles/pac12/themes/pac12_foundation/images/pac12/networks/network-bayarea.jpg', fanart, 'Pac-12 Bay Area')
-    addDir('Los Angeles', my_addon.getSetting("Angeles"), 1, 'http://x.pac-12.com/profiles/pac12/themes/pac12_foundation/images/pac12/networks/network-losangeles.jpg', fanart, 'Pac-12 Los Angeles')
-    addDir('Mountain', my_addon.getSetting("Mountain"), 1, 'http://x.pac-12.com/profiles/pac12/themes/pac12_foundation/images/pac12/networks/network-mountain.jpg', fanart, 'Pac-12 Mountain')
-    addDir('Oregon', my_addon.getSetting("Oregon"), 1, 'http://x.pac-12.com/profiles/pac12/themes/pac12_foundation/images/pac12/networks/network-oregon.jpg', fanart, 'Pac-12 Oregon')
-    addDir('Washington', my_addon.getSetting("Washington"), 1, 'http://x.pac-12.com/profiles/pac12/themes/pac12_foundation/images/pac12/networks/network-washington.jpg', fanart, 'Pac-12 Washington')
+    addDir('Pac National', 'p12netw', 1, icon, fanart, 'Pac-12')
+    addDir('Arizona', 'p12ariz', 1, 'http://x.pac-12.com/profiles/pac12/themes/pac12_foundation/images/pac12/networks/network-arizona.jpg', fanart, 'Pac-12 Arizona')
+    addDir('Bay Area', 'p12baya', 1, 'http://x.pac-12.com/profiles/pac12/themes/pac12_foundation/images/pac12/networks/network-bayarea.jpg', fanart, 'Pac-12 Bay Area')
+    addDir('Los Angeles', 'p12losa', 1, 'http://x.pac-12.com/profiles/pac12/themes/pac12_foundation/images/pac12/networks/network-losangeles.jpg', fanart, 'Pac-12 Los Angeles')
+    addDir('Mountain', 'p12moun', 1, 'http://x.pac-12.com/profiles/pac12/themes/pac12_foundation/images/pac12/networks/network-mountain.jpg', fanart, 'Pac-12 Mountain')
+    addDir('Oregon', 'p12oreg', 1, 'http://x.pac-12.com/profiles/pac12/themes/pac12_foundation/images/pac12/networks/network-oregon.jpg', fanart, 'Pac-12 Oregon')
+    addDir('Washington', 'p12wash', 1, 'http://x.pac-12.com/profiles/pac12/themes/pac12_foundation/images/pac12/networks/network-washington.jpg', fanart, 'Pac-12 Washington')
     if '//' in my_addon.getSetting("BTN"):
         addDir('BTN', my_addon.getSetting("BTN"), 2, iconbtn, fanartbtn, 'Big Ten')
 
@@ -82,18 +83,59 @@ def getItemTitles(table):
 
 
 def Play(url, name, icon):
-    lista = [['1080p', '4728.m3u8'], ['720p', '2328.m3u8'], ['SD', '1164.m3u8']]
-    lista2 = [['1080p', 'index_4728_av-p.m3u8'], ['720p', 'index_2328_av-b.m3u8'], ['SD', 'index_1164_av-b.m3u8']]
-    d = xbmcgui.Dialog()
-    item = d.select("Select Quality", getItemTitles(lista))
-    if item != -1:
-        if 'xrxs' in url:
-            url = url + str(lista[item][1])
-        else:
-            url = url + str(lista2[item][1])
-        li = xbmcgui.ListItem(label=name, iconImage=icon, thumbnailImage=icon, path="")
-        xbmc.Player().play(item=url, listitem=li)
-    exit()
+    if my_addon.getSetting("use_custom_links") == "true":
+        lista = [['1080p', '4728.m3u8'], ['720p', '2328.m3u8'], ['SD', '1164.m3u8']]
+        lista2 = [['1080p', 'index_4728_av-p.m3u8'], ['720p', 'index_2328_av-b.m3u8'], ['SD', 'index_1164_av-b.m3u8']]
+        d = xbmcgui.Dialog()
+        item = d.select("Select Quality", getItemTitles(lista))
+        if item != -1:
+            if 'p12netw' in url:
+                url = my_addon.getSetting("National")
+            elif 'p12ariz' in url:
+                url = my_addon.getSetting("Arizona")
+            elif 'p12baya' in url:
+                url = my_addon.getSetting("Bay")
+            elif 'p12losa' in url:
+                url = my_addon.getSetting("Angeles")
+            elif 'p12moun' in url:
+                url = my_addon.getSetting("Mountain")
+            elif 'p12oreg' in url:
+                url = my_addon.getSetting("Oregon")
+            elif 'p12wash' in url:
+                url = my_addon.getSetting("Washington")
+            print url
+            if 'xrxs' in url:
+                url = url + str(lista[item][1])
+            else:
+                url = url + str(lista2[item][1])
+            li = xbmcgui.ListItem(label=name, iconImage=icon, thumbnailImage=icon, path="")
+            xbmc.Player().play(item=url, listitem=li)
+        exit()
+    else:
+        lista = [['1080p', '4728.m3u8'], ['720p', '2328.m3u8'], ['SD', '1164.m3u8']]
+        d = xbmcgui.Dialog()
+        item = d.select("Select Quality", getItemTitles(lista))
+        if item != -1:
+            quality = str(lista[item][1])
+            query_args = {'page': 'links', 'network': url, 'bitrate': quality}
+            encoded_args = urllib.urlencode(query_args)
+            request = urllib2.Request(site)
+            request.add_header('User-agent', 'Mozilla/5.0')
+            response = urllib2.urlopen(request, encoded_args)
+            data = response.read()
+            response.close()
+            link = re.compile('<input value="(.+?)" class=').findall(data)
+            if len(link) > 0:
+                for url in link:
+                    url = url.replace('.m3u8.m3u8' , '.m3u8')
+                    li = xbmcgui.ListItem(label=name, iconImage=icon, thumbnailImage=icon, path="")
+                    xbmc.Player().play(item=url, listitem=li)
+                    exit()
+            else:
+                addonname = my_addon.getAddonInfo('name')
+                line1 = "Try custom links"
+                line2 = "Greetings, huball"
+                xbmcgui.Dialog().ok(addonname, line1, line2)
 
 
 def Playbtn(url, name, icon):

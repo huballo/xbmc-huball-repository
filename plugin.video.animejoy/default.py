33bf04406fe109e638dabdf6b21d12371fca4ba5
huball <pawel.malinski@sg.sangroup.pl>
2017-07-28 15:51:02 +0200

    fix

--- plugin.video.animejoy/default.py
+++
@@ -1,9 +0,0 @@
-import xbmcaddon
-import xbmcgui
-
-addon = xbmcaddon.Addon()
-addonname = addon.getAddonInfo('name')
-line1 = "Animejoy addon is now a part of Anime-IPTV addon."
-line2 = "You can find it in my repository."
-line3 = "Greetings, huball"
-xbmcgui.Dialog().ok(addonname, line1, line2, line3)

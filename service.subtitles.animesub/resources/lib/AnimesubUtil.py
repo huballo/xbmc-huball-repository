4b4b6dc26e4bbefb001f7ec22c241d688122a09c
XeonFX <kristpav@gmail.com>
2018-10-25 12:51:17 +0200

    uogólnienie przypadków testowych oraz poprawki w regexach

--- service.subtitles.animesub/resources/lib/AnimesubUtil.py
+++ service.subtitles.animesub/resources/lib/AnimesubUtil.py
@@ -4,7 +4,9 @@ import re
 
 class AnimesubUtil:
     def __init__(self, file_name):
-        self.__file = file_name.lower()
+        if not isinstance(file_name, str):
+            raise TypeError(file_name, "Must be str")
+        self.__file = file_name
 
     def searchable(self):
         title_episode = self.title()
@@ -17,16 +19,16 @@ class AnimesubUtil:
         return title_episode
 
     def episode(self):
-        episode = re.sub("[\s._-]\d{2}.\d{2,}", "", self.__file)
-        match = re.search("[\se_\[](?P<episode>\d{2,3})[\s._v\]]", episode)
+        episode = re.sub("[\s._-]\d{2}.\d{2,}", "", self.__file, flags=re.IGNORECASE)
+        match = re.search("[\se_\[](?P<episode>\d{2,3})[\s._v\]]", episode, re.IGNORECASE)
         if not match:
-            return False
+            return None
         return match.group('episode')
 
     def title(self):
         title = os.path.splitext(self.__file)[0]
-        title = re.sub("\[.*?\]|\(.*?\)|season|s\d{1,2}e\d{1,2}.*|s\d{1,2}", "", title)
-        match = re.findall("[a-z][\s.a-z_]*[a-z]", title)
+        title = re.sub("\[.*?\]|\(.*?\)|season|s\d{1,2}e\d{1,2}.*|s\d{1,2}|[^\x00-\x7F]+", "", title, flags=re.IGNORECASE)
+        match = re.findall("[a-zA-Z][a-zA-Z.-]*[_\sa-zA-Z]*[a-zA-Z]", title, re.IGNORECASE)
 
         if isinstance(match, list) and len(match) > 0:
             title = match[0]
@@ -40,6 +42,6 @@ class AnimesubUtil:
 
     @staticmethod
     def trim(value):
-        value = re.sub("-|\.+|\_", " ", value)
-        value = re.sub(" +", " ", value)
+        value = re.sub("-|\.+|\_", " ", value, flags=re.IGNORECASE)
+        value = re.sub(" +", " ", value, flags=re.IGNORECASE)
         return value.strip()
--- service.subtitles.animesub/resources/lib/TestAnimesubUtil.py
+++ service.subtitles.animesub/resources/lib/TestAnimesubUtil.py
@@ -5,122 +5,85 @@ from AnimesubUtil import AnimesubUtil
 
 
 class TestAnimesubUtil(TestCase):
+    def assertEpisode(self, expected, actual):
+        self.assertEqual(expected, AnimesubUtil(actual).episode())
+
     def assertTitle(self, expected, actual):
         self.assertEqual(expected, AnimesubUtil(actual).searchable())
 
-    def test_empty_string(self):
-        self.assertEqual("", "")
+    def test_episode_empty_string(self):
+        self.assertEpisode(None, "")
 
-    def test_none(self):
-        self.assertEqual(None, None)
+    def test_episode_none(self):
+        with self.assertRaises(TypeError):
+            self.assertEpisode(None, None)
 
-    def test_horriblesubs_shingeki_no_kyojin(self):
-        self.assertTitle("shingeki no kyojin ep40", "[HorribleSubs] Shingeki no Kyojin S3 - 40 [1080p].mkv")
+    def test_episode(self):
+        self.assertEpisode("01", "[Lorem] Ipsum Dolor Sit - 01 (1080p x265 10bit).mkv")
 
-    def test_horriblesubs_tensei_shitara_slime_datta_ken(self):
-        self.assertTitle("tensei shitara slime datta ken ep01", "[HorribleSubs] Tensei Shitara Slime Datta Ken - 01 [1080p].mkv")
+    def test_episode_in_brackets(self):
+        self.assertEpisode("01", "[Lorem] Ipsum Dolor Sit - [01] (1080p x265 10bit).mkv")
 
-    def test_horriblesubs_goblin_slayer(self):
-        self.assertTitle("goblin slayer ep01", "[HorribleSubs] Goblin Slayer - 01 [1080p].mkv")
+    def test_episode_in_brackets(self):
+        self.assertEpisode("01", "[Lorem] Ipsum Dolor Sit - [01v2] (1080p x265 10bit).mkv")
 
-    def test_horriblesubs_darling_in_the_franxx(self):
-        self.assertTitle("darling in the franxx ep01", "[HorribleSubs] Darling in the FranXX - 01 [1080p].mkv")
+    def test_episode_with_dot_in_brackets(self):
+        self.assertEpisode("01", "[Lorem] Ipsum Dolor Sit - [01.5] (1080p x265 10bit).mkv")
 
-    def test_horriblesubs_tokyo_ghoul_re(self):
-        self.assertTitle("tokyo ghoul re ep01", "[HorribleSubs] Tokyo Ghoul re - 01 [1080p].mkv")
+    def test_episode_with_dot_and_version_in_brackets(self):
+        self.assertEpisode("01", "[Lorem] Ipsum Dolor Sit - [01.5v2] (1080p x265 10bit).mkv")
 
-    def test_horriblesubs_sword_art_online(self):
-        self.assertTitle("sword art online ep01", "[HorribleSubs] Sword Art Online - 01 [1080p].mkv")
+    def test_episode_fps(self):
+        self.assertEpisode(None, "[Lorem] Ipsum Dolor Sit (1080p x265 23.976fps 10bit).mkv")
 
-    def test_horriblesubs_sword_art_online_ii(self):
-        self.assertTitle("sword art online ii ep01", "[HorribleSubs] Sword Art Online II - 01 [1080p].mkv")
+    def test_searchable_string(self):
+        self.assertTitle("", "")
 
-    def test_horriblesubs_sword_art_online_ii_half_episode(self):
-        self.assertTitle("sword art online ii ep14", "[HorribleSubs] Sword Art Online II - 14.5 [1080p].mkv")
+    def test_searchable_none(self):
+        with self.assertRaises(TypeError):
+            self.assertTitle(None, None)
 
-    def test_horriblesubs_sword_art_online_alicization(self):
-        self.assertTitle("sword art online ep01", "[HorribleSubs] Sword Art Online - Alicization - 01 [1080p].mkv")
+    def test_searchable_brackets(self):
+        self.assertTitle("Ipsum Dolor Sit ep01", "[Lorem] Ipsum Dolor Sit - 01 (1080p x265 10bit).mkv")
 
-    def test_horriblesubs_one_punch_man(self):
-        self.assertTitle("one punch man ep01", "[HorribleSubs] One-Punch Man - 01 [1080p].mkv")
+    def test_searchable_brackets_s1(self):
+        self.assertTitle("Ipsum Dolor Sit ep01", "[Lorem] Ipsum Dolor Sit S1 - 01 (1080p x265 10bit).mkv")
 
-    def test_oyhs_raws_tensei_shitara_slime_datta_ken(self):
-        self.assertTitle("tensei shitara slime datta ken ep01", "[Ohys-Raws] Tensei Shitara Slime Datta Ken - 01 (BS11 1280x720 x264 AAC).mp4")
+    def test_searchable_brackets_season1(self):
+        self.assertTitle("Ipsum Dolor Sit ep01", "[Lorem] Ipsum Dolor Sit Season 1 - 01 (1080p x265 10bit).mkv")
 
-    def test_oyhs_raws_golbin_slayer(self):
-        self.assertTitle("goblin slayer ep01", "[Ohys-Raws] Goblin Slayer - 01 (AT-X 1280x720 x264 AAC).mp4")
+    def test_searchable_brackets_half_episode(self):
+        self.assertTitle("Ipsum Dolor Sit ep01", "[Lorem] Ipsum Dolor Sit - 01.5 (1080p x265 10bit).mkv")
 
-    def test_zero_raws_fate_zero(self):
-        self.assertTitle("fate zero ep01", "[Zero-Raws] Fate Zero - 01 (BD 1920x1080 x264 FLAC).mkv")
+    def test_searchable_brackets_episode_name(self):
+        self.assertTitle("Ipsum Dolor Sit ep01", "[Lorem] Ipsum Dolor Sit - Amet - 01 (1080p x265 10bit).mkv")
 
-    def test_commie_banana_fish(self):
-        self.assertTitle("banana fish ep14", "[Commie] Banana Fish - 14 [F2EB6BF6].mkv")
+    def test_searchable_brackets_title_with_dash(self):
+        self.assertTitle("Ipsum Dolor Sit ep01", "[Lorem] Ipsum-Dolor Sit - 01 (1080p x265 10bit).mkv")
 
-    def test_utw_fate_apocrypha(self):
-        self.assertTitle("fate apocrypha ep01", "[UTW]_Fate_Apocrypha_-_01_[h264-720p][1D074A57].mkv")
+    def test_searchable_brackets_no_episode(self):
+        self.assertTitle("Ipsum Dolor Sit", "[Lorem] Ipsum Dolor Sit 2018 (1080p x265 10bit).mkv")
 
-    def test_fff_shingeki_no_kyojin(self):
-        self.assertTitle("shingeki no kyojin ep01", "[FFF] Shingeki no Kyojin - 01 [BD][1080p-FLAC][7655512C].mkv")
+    def test_searchable_brackets_and_underline_separator(self):
+        self.assertTitle("Ipsum Dolor Sit ep01", "[Lorem]_Ipsum_Dolor_Sit_-_01_(1080p-x265_10bit).mkv")
 
-    def test_dok_clannad_after_story(self):
-        self.assertTitle("clannad after story ep01", "[Doki] Clannad After Story - 01 (1920x1080 Hi10P BD FLAC) [1AA8D908].mkv")
+    def test_searchable_brackets_and_underline_separator_episode_titles(self):
+        self.assertTitle("Ipsum Dolor Sit ep01", "[Lorem]_Ipsum_Dolor_Sit_-_Session_01_-_Amet_[720p].mkv")
 
-    def test_coalgirls_no_game_no_life(self):
-        self.assertTitle("no game no life ep01", "[Coalgirls]_No_Game_No_Life_01_(1920x1080_Blu-Ray_FLAC)_[5673F6E8].mkv")
+    def test_searchable_brackets_title_starts_with_3_or_lower_chars(self):
+        self.assertTitle("Sed Ipsum Dolor Sit ep01", "[Lorem] Sed - Ipsum Dolor Sit - 01 (1080p x265 10bit).mkv")
 
-    def test_lns_code_geass_akito_the_exiled(self):
-        self.assertTitle("code geass ep01", "[LNS] Code Geass - Akito the Exiled - 01 [BD 720p] [34FE67E7].mkv")
+    def test_searchable_brackets_with_utf8_chars(self):
+        self.assertTitle("Ipsum Dolor Sit ep01", "[Lorem] Ipsum Dolor √Sit - 01 (1080p x265 10bit).mkv")
 
-    def test_weiss_made_in_abyss(self):
-        self.assertTitle("made in abyss ep01", "[WEISS] Made in Abyss 01.mkv")
+    def test_searchable_underline_s01e01(self):
+        self.assertTitle("Ipsum Dolor Sit ep01", "Ipsum_Dolor_Sit_S01E01_720p_BrRip_Lorem.mkv")
 
-    def test_reactor_great_teacher_onizuka(self):
-        self.assertTitle("gto great teacher onizuka ep01", "[Reaktor] GTO - Great Teacher Onizuka - E01 [576p][x265][10-bit].mkv")
+    def test_searchable_starts_with_title(self):
+        self.assertTitle("Ipsum Dolor Sit ep01", "Ipsum Dolor Sit - 01 [720p].mkv")
 
-    def test_beatrice_raws_shingeki_no_kyojin(self):
-        self.assertTitle("shingeki no kyojin ep01", "[Beatrice-Raws] Shingeki no Kyojin Season 2 - 01 [BDRip 1920x1080 x264 FLAC].mkv")
+    def test_searchable_starts_with_title_ends_with_episode(self):
+        self.assertTitle("Ipsum Dolor Sit ep01", "Ipsum Dolor Sit - 01.mkv")
 
-    def test_noobsubs_tokyo_ghoul(self):
-        self.assertTitle("tokyo ghoul ep04", "[NoobSubs] Tokyo Ghoul 04 (1080p Blu-ray Dual Audio 8bit AAC)[564EAF1D].mkv")
-
-    def test_noobsubs_tokyo_ghoul_a(self):
-        self.assertTitle("tokyo ghoul ep01", "[NoobSubs] Tokyo Ghoul √A 01 (1080p Blu-ray Dual Audio 8bit AAC)[FA0B064F].mkv")
-
-    def test_cbm_cowboy_bebop(self):
-        self.assertTitle("cowboy bebop ep01", "[CBM]_Cowboy_Bebop_-_Session_01_-_Asteroid_Blues_[720p]_[ACFADD3A].mkv")
-
-    def test_hysub_saenai_heroine_no_sodatekata(self):
-        self.assertTitle("saenai heroine no sodatekata ep10", "[HYSUB]Saenai Heroine no Sodatekata ♭[10][GB_MP4][1280X720].mp4")
-
-    def test_succ_kimi_no_na_wa(self):
-        self.assertTitle("kimi no na wa", "Kimi.no.Na.wa.2016.1080p.Remux.AVC.FLAC.5.1-SUCC.mkv")
-
-    def test_35mm_koe_no_katachi(self):
-        self.assertTitle("koe no katachi", "[35mm] Koe no Katachi - A Silent Voice [1080p] [B9471AD7].mkv")
-
-    def test_mabors_vcb_studio_shigatsu_wa_kimi_no_uso(self):
-        self.assertTitle("shigatsu wa kimi no uso ep04", "[Mabors&VCB-Studio] Shigatsu wa Kimi no Uso [04][Hi10p_1080p][x264_2flac].mkv")
-
-    def test_mabors_vcb_studio_shigatsu_wa_kimi_no_uso_v2(self):
-        self.assertTitle("shigatsu wa kimi no uso ep09", "[Mabors&VCB-Studio] Shigatsu wa Kimi no Uso [09v2][Hi10p_1080p][x264_2flac].mkv")
-
-    def test_pophd_evangelion_111(self):
-        self.assertTitle("evangelion", "Evangelion 1.11 You Are (Not) Alone (2007) MULTi [1080p] BluRay x264-PopHD.mkv")
-
-    def test_fch1993_clannad_movie(self):
-        self.assertTitle("clannad", "CLANNAD [DVD 720x480 23.976fps AVC-yuv420p10 FLAC] - fch1993.mkv")
-
-    def test_tinymz_steins_gate(self):
-        self.assertTitle("steins gate ep01", "Steins_Gate_S01E01_720p_BrRip_TINYMZ.mkv")
-
-    def test_unknown_steins_gate_0(self):
-        self.assertTitle("steins gate ep01", "Steins Gate 0 - 01 [720p].mkv")
-
-    def test_fardadl_tokyo_ghoul_s01_ep01(self):
-        self.assertTitle("tokyo ghoul ep01", "Tokyo.Ghoul.S01E01.Tragedy.1080p.x265.FardaDL.mkv")
-
-    def test_fardadl_tokyo_ghoul_s02_ep06(self):
-        self.assertTitle("tokyo ghoul ep06", "Tokyo.Ghoul.S02E06.1080p.x265.Pseudo.FardaDL")
-
-    def test_fardadl_tokyo_ghoul_s03_ep01(self):
-        self.assertTitle("tokyo ghoul ep01", "Tokyo.Ghoul.S03E01.Those.Who.Hunt.START.1080p.x265.10bit.Dual.Audio.DedSec.FardaDL")
+    def test_searchable_dots_episode_title(self):
+        self.assertTitle("Ipsum Dolor Sit ep01", "Ipsum.Dolor.Sit.S01E01.Amet.1080p.x265.Lorem.mkv")
